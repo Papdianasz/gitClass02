45a8c2bf200916126911e3a8dd799dd295f681bc
Papdianasz <papdianasz@gmail.com>
2025-05-06 21:03:25 -0400

    first commit

---
+++ src/test/java/Test.java
@@ -0,0 +1,21 @@
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello word");
+        System.out.println("This is a project version 1");
+    }
+}
+
+//Done with version one
+
+//Staging area is an intermediate area between the directory and a Repo
+//it is a temporary staging area
+//Working directory is a folder in which we have all the project files like SRC,.idea POM and inside this area we have the staging area and the repo
+// if you want all the files and folders(SRC, .idea, POM) inside a Repo use git add. command
+//it will make a snapshot  of all files and folders that are inside the project and it's going to place them into staging area
+//if you run the command git commit-m "1stname" this will grab the file and folders from the staging area and move to the Repo
+//Git AKA 3tage architecture (working directory->staging area->Repo)
+// with git status command t can check is there any file in my repo
+//
+
+
+
