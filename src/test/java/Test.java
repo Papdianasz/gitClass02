7ca6abed7974850d8db40be2af3883d0ebe7a540
Papdianasz <papdianasz@gmail.com>
2025-05-06 21:19:59 -0400

    second commit

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,7 +1,16 @@
 public class Test {
     public static void main(String[] args) {
+
+
         System.out.println("hello word");
         System.out.println("This is a project version 1");
+        System.out.println("git add.");
+        System.out.println("git commit -m message");
+        System.out.println("git status");
+        System.out.println("git init");
+        System.out.println("git log");
+        System.out.println("pwd, ls, cd");
+
     }
 }
 
@@ -12,10 +21,17 @@ public class Test {
 //Working directory is a folder in which we have all the project files like SRC,.idea POM and inside this area we have the staging area and the repo
 // if you want all the files and folders(SRC, .idea, POM) inside a Repo use git add. command
 //it will make a snapshot  of all files and folders that are inside the project and it's going to place them into staging area
-//if you run the command git commit-m "1stname" this will grab the file and folders from the staging area and move to the Repo
+//if you run the command git commit -m "1stname" this will grab all the files and folders from the staging area and move to the Repo
 //Git AKA 3tage architecture (working directory->staging area->Repo)
 // with git status command t can check is there any file in my repo
+//git log command is help us to see how many commit we have made in our repo
+
+
+//done with version two
+
 //
+
+
 
 
 
